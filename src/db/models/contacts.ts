944bcd39a25c3ad0d0c75c7aaf1af51c46a8fb5a
edard3v <edard3v@gmail.com>
2025-02-17 17:48:19 -0500

    agg contacto funcional

--- src/db/models/contacts.ts
+++ src/db/models/contacts.ts
@@ -1,22 +1,30 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text, uniqueIndex } from "drizzle-orm/sqlite-core";
 import { accounts } from "./accounts.ts";
 
-export const contacts = sqliteTable("contacts", {
-  id: text({ length: 36 })
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: text().notNull(),
-  tel: integer().notNull(),
-  country: integer().notNull(), // recuerda validar con el enum Country
+export const contacts = sqliteTable(
+  "contacts",
+  {
+    id: text({ length: 36 })
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: text().notNull(),
+    tel: integer().notNull(),
+    country: integer().notNull(), // recuerda validar con el enum Country
 
-  created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
-  updated_at: text().$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
+    created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
+    updated_at: text().$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
 
-  account_id: text()
-    .notNull()
-    .references(() => accounts.id, { onDelete: "cascade" }),
-});
+    account_id: text()
+      .notNull()
+      .references(() => accounts.id, { onDelete: "cascade" }),
+  },
+
+  (t) => [
+    uniqueIndex("unique_contact_per_account").on(t.account_id, t.name, t.tel),
+    // Esto evita que una misma cuenta tenga contactos duplicados por nombre o tel
+  ]
+);
 
 export type InsertContacts = typeof contacts.$inferInsert;
 export type SelectContacts = typeof contacts.$inferSelect;
