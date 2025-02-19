5ae6f2b5329837a7902e0c69fe7dd73c49bcd586
edard3v <edard3v@gmail.com>
2025-02-19 00:57:37 -0500

    agg console.log

--- src/modules/auth/end_register/end_register_service.ts
+++ src/modules/auth/end_register/end_register_service.ts
@@ -3,5 +3,6 @@ import { accounts } from "../../../db/schema.ts";
 import { StartRegisterDto } from "../start_register/start_register_dto.ts";
 
 export const end_register_service = async (newAccount: StartRegisterDto) => {
+  console.log("***********Registering*********");
   await db.insert(accounts).values({ ...newAccount });
 };
