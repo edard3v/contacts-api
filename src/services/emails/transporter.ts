37d58e319409beda5b0ff2f6959a2f14632dda8f
edard3v <edard3v@gmail.com>
2025-02-18 21:25:54 -0500

    refactor config nodemailer

--- src/services/emails/transporter.ts
+++ src/services/emails/transporter.ts
@@ -2,10 +2,13 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587, // Cambiado de 465 a 587
+  secure: false, // IMPORTANTE: false para usar STARTTLS
   auth: {
     user: Deno.env.get("NODEMAILER_GMAIL"),
     pass: Deno.env.get("NODEMAILER_GMAIL_APP_PASSWORD"),
+  },
+  tls: {
+    rejectUnauthorized: false, // Opcional si hay problemas con certificados
   },
 });
