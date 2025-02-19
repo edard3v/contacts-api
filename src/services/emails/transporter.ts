b24fea9a456df120ecbd44606931754daad9676a
edard3v <edard3v@gmail.com>
2025-02-18 21:36:00 -0500

    refactor

--- src/services/emails/transporter.ts
+++ src/services/emails/transporter.ts
@@ -3,12 +3,8 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
 
-  secure: false, // IMPORTANTE: false para usar STARTTLS
   auth: {
     user: Deno.env.get("NODEMAILER_GMAIL"),
     pass: Deno.env.get("NODEMAILER_GMAIL_APP_PASSWORD"),
-  },
-  tls: {
-    rejectUnauthorized: false, // Opcional si hay problemas con certificados
   },
 });
