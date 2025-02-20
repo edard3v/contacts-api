773e2fff5082c37db3957d1c50c72d29efef5632
edard3v <edard3v@gmail.com>
2025-02-20 00:49:05 -0500

    refactor

--- src/app/err_handler.ts
+++ src/app/err_handler.ts
@@ -3,10 +3,10 @@ import { HTTPException } from "hono/http-exception";
 
 export const err_handler: ErrorHandler = (err, context) => {
   if (err instanceof HTTPException) {
-    return context.json({ error: err.message, status: err.status });
+    return err.getResponse();
   }
 
   console.log(err);
   context.status(500);
-  return context.json({ err_name: err.name, msg: "Error de servidor" });
+  return context.text(err.name);
 };
