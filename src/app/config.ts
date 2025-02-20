5d5f061a28db29f304c6806236f409b9deac0c73
edard3v <edard3v@gmail.com>
2025-02-20 00:50:56 -0500

    fix cors

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,8 +3,8 @@ import { cors } from "hono/cors";
 export const PORT = Number(Deno.env.get("PORT")) || 3001;
 export const IS_PRODUCTION = Deno.env.get("ENV") === "production";
 export const CORS = cors({
-  origin: ["*"],
-  allowMethods: ["POST", "GET", "UPDATE", "PUT", "DELETE"],
+  origin: "*",
+  allowMethods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
   maxAge: 600,
   credentials: true,
 });
